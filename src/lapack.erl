af97973af27e4a1716f30c94f09acf5b7f87464f
Simon Klassen <simon@io>
2014-06-02 21:46:55 +0800

    Added eunit test

--- src/lapack.erl
+++ src/lapack.erl
@@ -1,20 +1,9 @@
-%% Copyright 2011,  Filipe David Manana  <fdmanana@apache.org>
-%% Web:  http://github.com/fdmanana/lapack-erlang-nif
-%%
-%% Licensed under the Apache License, Version 2.0 (the "License"); you may not
-%% use this file except in compliance with the License. You may obtain a copy of
-%% the License at
-%%
-%%  http://www.apache.org/licenses/LICENSE-2.0
-%%
-%% Unless required by applicable law or agreed to in writing, software
-%% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-%% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-%% License for the specific language governing permissions and limitations under
-%% the License.
-
 -module(lapack).
 -export([svd/1,mmultiply/2]).
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
 
 -on_load(init/0).
 
@@ -40,4 +29,11 @@ init() ->
 svd(_) -> exit(nif_library_not_loaded).
 mmultiply(_,_) -> exit(nif_library_not_loaded).
 
+-ifdef(TEST).
 
+simple_test() ->
+	A=[[1.0,2.0],[3.0,4.0]],
+	B=[[1.0,2.0,3.0],[4.0,5.0,6.0]],
+	?assert(lapack:mmultiply(A,B)==[[9.0,12.0,15.0],[19.0,26.0,33.0]]).
+
+-endif.
